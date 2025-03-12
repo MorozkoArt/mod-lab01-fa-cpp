39e76df1c64a6a2ef68e92dde98ae28ccb3328df
Morozov Artem <artemmorozov155@gmail.com>
2025-03-12 14:07:29 +0300

    changing the code format for google test

--- include/fun.h
+++ include/fun.h
@@ -1,9 +1,13 @@
 // Copyright 2022 UNN-IASR
 #ifndef INCLUDE_FUN_H_
 #define INCLUDE_FUN_H_
+
 #include <cstring> 
+
 #include <cctype>
+
 #include <cmath>  
+
 unsigned int faStr1(const char *str);
 unsigned int faStr2(const char *str);
 unsigned int faStr3(const char *str);
--- src/fun.cpp
+++ src/fun.cpp
@@ -21,7 +21,7 @@ unsigned int faStr1(const char *str) {
         if (std::isspace(*str) && inWord) {
             containsDigit = false;
             inWord = false;
-        }     
+        }
         str++;
     }
     return count;
@@ -35,15 +35,13 @@ unsigned int faStr2(const char *str) {
         return 0;
     }
     while (*str) {
-        if(std::isspace(*str) && inWord) {
+        if (std::isspace(*str) && inWord) {
             validWord = false;
             inWord = false;
-        }
-        else if (!std::islower(*str) && validWord) {
+        } else if (!std::islower(*str) && validWord) {
             count--;
             validWord = false;
-        }
-        else if (!inWord && std::isupper(*str)) {
+        } else if (!inWord && std::isupper(*str)) {
             count++;
             inWord = true;
             validWord = true;
@@ -63,19 +61,18 @@ unsigned int faStr3(const char *str) {
     while (*str) {
         if (std::isspace(*str) && inWord) {
             inWord = false;
-        }
-        else if (!std::isspace(*str) && !inWord) {
+        } else if (!std::isspace(*str) && !inWord) {
             inWord = true;
             count++;
             lenWords++;
-        }
-        else if (!std::isspace(*str) && inWord) {
+        } else if (!std::isspace(*str) && inWord) {
             lenWords++;
-        }          
+        }
         str++;
     }
-    if (count==0) {
+    if (count == 0) {
         return 0;
     }
-    return static_cast<unsigned int>(std::round(static_cast<double>(lenWords) / count));
+    return (static_cast<unsigned int>
+        (std::round(static_cast<double>(lenWords) / count)));
 }
